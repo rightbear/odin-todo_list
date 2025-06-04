ef71207afeb4eeaf4d2375ca05796e86f0520068
rightbear <jjj17041113@gmail.com>
2025-06-04 16:54:18 +0800

    style: add CSS layout to project titles in sidebar

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ itemLogic.addTask(1, "task10", "des10", "due10", "pri10", "project2", "note10", 
 
 //itemLogic.deleteTask(0, 0);
 
-itemLogic.modifyProject(1, "project233333", "des2", "note2");
+itemLogic.modifyProject(1, "ddddddddd555d", "des2", "note2");
 
 //itemLogic.deleteProject(0);
 
